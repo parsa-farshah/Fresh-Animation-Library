a3765ab040607495808cc4bc75b1b3646c2d2d43
parsa <parsafarashah@gmail.com>
2025-10-08 01:14:32 +0330

    first commit

---
+++ src/js/master.js
@@ -0,0 +1,29 @@
+// select div s want to get animation
+let selectDiv = document.querySelectorAll(".freshAnimation");
+
+selectDiv.forEach((val) => {
+  let classAnimation = val.getAttribute(
+    "data-freshAnimation",
+    "data-freshAnimation"
+  );
+  val.classList.add(classAnimation);
+  val.setAttribute("data-top", val.offsetTop);
+  let timeAnimation = val.getAttribute(
+    "freshAnimationTime",
+    "freshAnimationTime"
+  );
+
+  window.addEventListener("scroll", () => {
+    let st = parseInt(window.scrollY);
+    if (
+      st + 400 > val.getAttribute("data-top", "data-top") 
+    ) {
+      val.classList.remove(classAnimation);
+      val.classList.add(`duration-${timeAnimation}`);
+    } else {
+      val.classList.add(classAnimation);
+      val.classList.remove(`duration-${timeAnimation}`);
+      console.log("hloo");
+    }
+  });
+});
